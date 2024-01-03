da05e3c0b0ed8cbbb6012440d9c1e2ef1e67d5fa
hassan5544 <hmh.lol55@gmail.com>
2024-01-03 21:10:31 +0300

    Add project files.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.WithOrigins("https://example.com")
+                .WithMethods("POST", "GET")
+                .WithHeaders("accept", "content-type"));
+            });
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+            });
+
+    }
+}
